661b9f1c7c64d94a6223861cc8d383cd2487d653
gnewton <glen.newton@gmail.com>
2018-04-08 02:32:03 -0400

    Added channel support

--- grater.go
+++ grater.go
@@ -13,12 +13,11 @@ import (
 )
 
 type Grater struct {
-	ticker *time.Ticker
-	name   string
-	writer *bufio.Writer
-	outFunc      outFunc
+	ticker  *time.Ticker
+	name    string
+	writer  *bufio.Writer
+	outFunc outFunc
 }
-
 
 func NewFileGrater(d time.Duration, counter *uint64, w *bufio.Writer) (*Grater, error) {
 	return graterInit(d, counter, writerOut, w)
@@ -41,10 +40,10 @@ func graterInit(d time.Duration, counter *uint64, o outFunc, w *bufio.Writer) (*
 		return nil, errors.New("Error: Duration must be >0")
 	}
 
-	if o == nil{
+	if o == nil {
 		return nil, errors.New("outFunc is nil")
 	}
-	
+
 	grater := new(Grater)
 	grater.outFunc = o
 	grater.writer = w
@@ -55,7 +54,6 @@ func graterInit(d time.Duration, counter *uint64, o outFunc, w *bufio.Writer) (*
 	return grater, nil
 }
 
-
 func counterTicker(r *Grater, counter *uint64) {
 	if r == nil || counter == nil {
 		log.Fatal("Grater or counter are nil")
@@ -65,7 +63,7 @@ func counterTicker(r *Grater, counter *uint64) {
 
 	for _ = range r.ticker.C {
 		err := r.outFunc(r, strconv.FormatUint(*counter-previousCounter, 10))
-		if err != nil{
+		if err != nil {
 			log.Fatal("Grater or counter are nil")
 		}
 		previousCounter = *counter
@@ -73,3 +71,45 @@ func counterTicker(r *Grater, counter *uint64) {
 
 }
 
+func channelTicker(r *Grater, c chan struct{}) {
+	if r == nil || c == nil {
+		log.Fatal("Grater or channel are nil")
+	}
+
+	for _ = range r.ticker.C {
+		err := r.outFunc(r, r.name+"::"+strconv.Itoa(len(c)))
+		if err != nil {
+			log.Fatal("Grater or counter are nil")
+		}
+
+	}
+
+}
+
+func NewChannelGrater(name string, d time.Duration, c chan struct{}) (*Grater, error) {
+	return graterChannelInit(name, d, c, logOut, nil)
+}
+
+func graterChannelInit(name string, d time.Duration, c chan struct{}, o outFunc, w *bufio.Writer) (*Grater, error) {
+	if int64(d) <= 0 {
+		return nil, errors.New("Error: Duration must be >0")
+	}
+
+	if c == nil {
+		return nil, errors.New("Error: channel is nil")
+	}
+
+	if o == nil {
+		return nil, errors.New("outFunc is nil")
+	}
+
+	grater := new(Grater)
+	grater.outFunc = o
+	grater.writer = w
+	grater.ticker = time.NewTicker(d)
+	grater.name = name
+
+	go channelTicker(grater, c)
+
+	return grater, nil
+}
